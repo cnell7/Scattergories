0670cf19cd191996d1f6894f1c00903057a3a286
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-17 14:10:50 -0500

    Add mongojs

--- scattergories/server.js
+++ scattergories/server.js
@@ -1,20 +1,13 @@
 const express =  require('express');
-const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+const mongojs = require('mongojs');
+const db = mongojs('mongodb+srv://cnell:r[~7vn&seP_DV&=t@cluster0.tak5v.mongodb.net/scattergories?retryWrites=true&w=majority');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const mongoDB_URI = 'mongodb+srv://cnell:r[~7vn&seP_DV&=t@cluster0.tak5v.mongodb.net/scattergories?retryWrites=true&w=majority';
 
-mongoose.connect(mongoDB_URI || 'mongodb://localhost/scattergories', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected.')
-})
 
 //HTTP request logger
 app.use(morgan('tiny'));
