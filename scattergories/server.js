d72260de1f36293d347c1d178e5a8744cf2a1525
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-17 13:46:37 -0500

    Add mongoose connection

--- scattergories/server.js
+++ scattergories/server.js
@@ -4,6 +4,34 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
+const mongoDB_URI = 'mongodb+srv://cnell:r[~7vn&seP_DV&=t@cluster0.tak5v.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+mongoose.connect('mongodb://localhost/full/scattergories', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected.')
+})
+
+//HTTP request logger
+app.use(morgan('tiny'));
+
+//Routes
+app.get('/api', (req, res) => {
+    const data = {
+        username: '',
+        age:5
+    }
+    res.json(data);
+})
+
+app.get('/api/name', (req, res) => {
+    const data = {
+
+    }
+})
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
