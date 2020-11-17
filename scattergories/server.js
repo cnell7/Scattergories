e48a726735debc8422862bbd51727dac71ab20e5
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-17 13:17:22 -0500

    Update react and server layout

---
+++ scattergories/server.js
@@ -0,0 +1,9 @@
+const express =  require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
