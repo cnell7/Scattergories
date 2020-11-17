1b6ab5cc7ae3696fa176d8a0c721dc67e5ee97f6
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-17 17:06:03 -0500

    Add login stuff from kmp example

--- scattergories/server.js
+++ scattergories/server.js
@@ -1,19 +1,137 @@
   
 const express = require('express');
-const path = require('path');
-const app = express();
-
-const Book = require('./Example.js');
-
+const expressSession = require('express-session');
 const bodyParser = require('body-parser');
+const path = require('path');
+const Example = require('./Example.js');
+const Secret = require("./Secret.js");
+const app = express();
+const login_data = require('data-store')({ path: process.cwd() + '/data/users.json' });
+
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
+app.use(expressSession({
+    name: "kmpSessionCookie",
+    secret: "express session secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.get('/example', (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "src", "Index.js"));
+    res.json(Example.getAllIDs());
     return;
 });
+
+app.post('/login', (req,res) => {
+
+    let user = req.body.user;
+    let password = req.body.password;
+
+    let user_data = login_data.get(user);
+    if (user_data == null) {
+        res.status(404).send("Not found");
+        return;
+    }
+    if (user_data.password == password) {
+        console.log("User " + user + " credentials valid");
+        req.session.user = user;
+        res.json(true);
+        return;
+    }
+    res.status(403).send("Unauthorized");
+});
+
+app.get('/logout', (req, res) => {
+    delete req.session.user;
+    res.json(true);
+})
+
+app.get('/secret', (req, res) => {
+    if (req.session.user == undefined) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    res.json(Secret.getAllIDsForOwner(req.session.user));
+    return;
+});
+
+app.get('/secret/:id', (req, res) => {
+    if (req.session.user == undefined) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    let s = Secret.findByID(req.params.id);
+    if (s == null) {
+        res.status(404).send("Not found");
+        return;
+    }
+
+    if (s.owner != req.session.user) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    res.json(s);
+} );
+
+app.post('/secret', (req, res)=> {
+    if (req.session.user == undefined) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    let s = Secret.create(req.session.user, req.body.secret);
+    if (s == null) {
+        res.status(400).send("Bad Request");
+        return;
+    }
+    return res.json(s);
+});
+
+app.put('/secret/:id', (req, res) => {
+    if (req.session.user == undefined) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    let s = Secret.findByID(req.params.id);
+    if (s == null) {
+        res.status(404).send("Not found");
+        return;
+    }
+    if (s.owner != req.session.user) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+    s.update(req.body.secret);
+
+    res.json(s.id);
+});
+
+app.delete('/secret/:id', (req, res) => {
+    if (req.session.user == undefined) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    let s = Secret.findByID(req.params.id);
+    if (s == null) {
+        res.status(404).send("Not found");
+        return;
+    }
+
+    if (s.owner != req.session.user) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    s.delete();
+    res.json(true);
+})
 
 const port = 3030;
 app.listen(port, () => {
