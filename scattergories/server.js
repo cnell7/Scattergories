5e17acd8c7a7c54fd8a0f92d51dc3eb4a3331c9e
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-17 16:00:17 -0500

    Working backend with static build

--- scattergories/server.js
+++ scattergories/server.js
@@ -1,6 +1,6 @@
   
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 const Book = require('./Example.js');
@@ -8,8 +8,10 @@ const Book = require('./Example.js');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, './client/build')));
+
 app.get('/example', (req, res) => {
-    res.json(Book.getAllIDs());
+    res.sendFile(path.join(__dirname, "client", "src", "Index.js"));
     return;
 });
 
