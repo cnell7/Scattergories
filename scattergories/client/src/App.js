db59d91546dcc91c56ec9ae333e2215d879133b0
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-18 13:19:34 -0500

    Add new navbar

--- scattergories/client/src/App.js
+++ scattergories/client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Home from "./Pages/Home.js"
 import Signup from "./Pages/Signup.js"
 import Login from "./Pages/Login.js"
 import {
@@ -12,15 +13,65 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+              <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"></img>
+            </a>
+
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item">
+                Home
+              </a>
+
+              <a class="navbar-item">
+                Documentation
+              </a>
+
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                  More
+                </a>
+
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">
+                    About
+                  </a>
+                  <a class="navbar-item">
+                    Jobs
+                  </a>
+                  <a class="navbar-item">
+                    Contact
+                  </a>
+                  <hr class="navbar-divider"></hr>
+                  <a class="navbar-item">
+                    Report an issue
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <a class="button is-primary">
+                    <Link to="/signup"><strong>Signup</strong></Link>
+                  </a>
+                  <a class="button is-light">
+                    <Link to="/login"><strong>Login</strong></Link>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
