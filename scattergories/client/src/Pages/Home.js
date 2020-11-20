7221d4f9ee71e240829b7323ce3e4f34d7dc2b26
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-20 09:12:18 -0500

    Add create and join game forms to home page

--- scattergories/client/src/Pages/Home.js
+++ scattergories/client/src/Pages/Home.js
@@ -2,9 +2,33 @@ import React from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom';
 
+export class GameForm extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  getState(){
+    let state = this.props.state.toString();
+    if(state == 'null'){
+      return;
+    }else if(state == 'create'){
+      return(<p>create</p>);
+    } else {
+      return(<p>join</p>);
+    }
+  }
+  render(){
+    return(this.getState());
+  }
+}
+
 export default class Home extends React.Component {
     constructor(props){
         super(props);
+        this.state = {renderedForm: 'null'}
+        this.setState = this.setState.bind(this);
+      }
+      setState(s){
+        this.state.renderedForm = s;
       }
       loggedOut(){
         return(
@@ -24,10 +48,11 @@ export default class Home extends React.Component {
           <h1 class='title is-1 has-text-centered'><strong>Click a button below to play.</strong></h1>
           <div id='homeContainer' class='container'>
             <div class='buttons is-grouped has-background-danger is-centered'>
-              <button class="button is-danger is-inverted">Create</button>
-              <button class="button is-danger is-inverted">Join</button>
+              <button class="button is-danger is-inverted" onClick='e.preventDefault(); this.setState("create")'>Create</button>
+              <button class="button is-danger is-inverted" onClick='e.preventDefault(); this.setState("join")'>Join</button>
             </div>
           </div>
+          <GameForm state={this.state.renderedForm}/>
         </div>);
       }
       getState(){
