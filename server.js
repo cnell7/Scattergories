709384be1cfac85a6c0a8f22374383fe3bcce3d9
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-21 10:38:15 -0500

    Remove test restful endpoints

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const server = http.createServer(app);
 const io = require('socket.io').listen(server);
 const expressSession = require('express-session');
 const bodyParser = require('body-parser');
-const path = require('path');
-const Secret = require("./Secret.js");
+econst path = require('path');
 const User = require("./User.js")
 const port = process.env.PORT || 3030
 const GameManager = require('./engine/GameManager')
@@ -73,92 +72,6 @@ app.get('/logout', (req, res) => {
     delete req.session.user;
     res.json(true);
 })
-
-app.get('/secret', (req, res) => {
-    if (req.session.user == undefined) {
-        res.status(403).send("Unauthorized");
-        return;
-    }
-
-    res.json(Secret.getAllIDsForOwner(req.session.user));
-    return;
-});
-
-app.get('/secret/:id', (req, res) => {
-    if (req.session.user == undefined) {
-        res.status(403).send("Unauthorized");
-        return;
-    }
-
-    let s = Secret.findByID(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
-
-    if (s.owner != req.session.user) {
-        res.status(403).send("Unauthorized");
-        return;
-    }
-
-    res.json(s);
-} );
-
-app.post('/secret', (req, res)=> {
-    if (req.session.user == undefined) {
-        res.status(403).send("Unauthorized");
-        return;
-    }
-
-    let s = Secret.create(req.session.user, req.body.secret);
-    if (s == null) {
-        res.status(400).send("Bad Request");
-        return;
-    }
-    return res.json(s);
-});
-
-app.put('/secret/:id', (req, res) => {
-    if (req.session.user == undefined) {
-        res.status(403).send("Unauthorized");
-        return;
-    }
-
-    let s = Secret.findByID(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
-    if (s.owner != req.session.user) {
-        res.status(403).send("Unauthorized");
-        return;
-    }
-    s.update(req.body.secret);
-
-    res.json(s.id);
-});
-
-app.delete('/secret/:id', (req, res) => {
-    if (req.session.user == undefined) {
-        res.status(403).send("Unauthorized");
-        return;
-    }
-
-    let s = Secret.findByID(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
-
-    if (s.owner != req.session.user) {
-        res.status(403).send("Unauthorized");
-        return;
-    }
-
-    s.delete();
-    res.json(true);
-})
-
 
 server.listen(port, () => {
     console.log("Scattergories up at " + port);
