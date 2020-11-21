f500d89d424d9e66e4e4bab7635d84029fd87805
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-20 22:30:05 -0500

    Fix request URL's

--- client/src/Services/LogoutService.js
+++ client/src/Services/LogoutService.js
@@ -3,7 +3,7 @@ import * as axios from "../../node_modules/axios"
 export async function requestLogout() {
     const result = await axios({
         method: 'get',
-        url: 'http://localhost:3030/logout'
+        url: 'https://scattergories-app.herokuapp.com/logout'
     }).catch(err => {
         return false;
     })
