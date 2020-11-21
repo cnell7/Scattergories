42ea2b13f81e6dde184895200f6fd5b82e74be86
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-21 10:42:06 -0500

    Fix urls

--- client/src/Services/SignupService.js
+++ client/src/Services/SignupService.js
@@ -3,7 +3,7 @@ import * as axios from "../../node_modules/axios"
 export async function requestSignup(u, p) {
     const result = await axios({
         method: 'post',
-        url: 'https://localhost:3030/signup',
+        url: 'http://localhost:3030/signup',
         data: {
             user: u,
             password: p
