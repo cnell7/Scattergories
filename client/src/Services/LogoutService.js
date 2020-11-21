0dd1898ac2f1482dbf0791148429a886ab19b802
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-21 10:41:32 -0500

    Fix URLs

--- client/src/Services/LogoutService.js
+++ client/src/Services/LogoutService.js
@@ -3,7 +3,7 @@ import * as axios from "../../node_modules/axios"
 export async function requestLogout() {
     const result = await axios({
         method: 'get',
-        url: 'https://localhost:3030/logout'
+        url: 'http://localhost:3030/logout'
     }).catch(err => {
         return false;
     })
