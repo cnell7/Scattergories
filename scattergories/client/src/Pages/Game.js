4b3eb88906ca58bc556a41c02ea19384779402e4
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-20 19:02:32 -0500

    Game styling and setup

--- scattergories/client/src/Pages/Game.js
+++ scattergories/client/src/Pages/Game.js
@@ -18,22 +18,19 @@ export default class Game extends React.Component {
                     <h1>Letter</h1>
                     <h2 class='title is-1'>{this.letter}</h2>
                 </div>
-              <ul>
-                  {this.players.map(player => {
-                      return <li>{player}</li>
-                  })}
-              </ul>
+                <button class="button">Play</button>
             </div>
             <div class="column is-two-fifths">
                 {this.categories.map(cat => {
-                    return <h1 class='title is-3'>{cat}</h1>
+                    return <input class='input' placeholder={cat}></input>
                 })}
-                <div>
-                    <input class='input' placeholder='Type your answer here.'></input>
-                </div>
             </div>
             <div class="column is-one-fifth">
-                <button class="button">Play</button>
+                <ul>
+                    {this.players.map(player => {
+                        return <li>{player}</li>
+                    })}
+                </ul>
             </div>
           </div>
         );
