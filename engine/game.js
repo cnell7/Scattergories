08533790ab77c4dc2f3a1f05e498d2e8febedd0b
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-22 18:04:27 -0500

    Server voting round incrementing

--- engine/game.js
+++ engine/game.js
@@ -20,7 +20,7 @@ class Game {
         this.host = ""
         this.playerAnswers = {}
         this.currentVotingRound = 0
-        this.incomingVotes = {}
+        this.incomingVotes = [];
     }
 
     getGameID() {
