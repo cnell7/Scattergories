2074893966b58215bb6bd79ade87cdc41af711f7
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-22 12:38:24 -0500

    Center ui elements

--- client/src/Pages/Game.js
+++ client/src/Pages/Game.js
@@ -14,14 +14,14 @@ export default class Game extends React.Component {
         <div class="columns">
             <div class="column is-one-fifths">
                 <div class="container">
-                    <h1 class='title is-3'>Letter</h1>
-                    <h1 id='gameLetter' class='title is-1'></h1>
-                    <h1 id="time" class="title is-4">90</h1>
+                    <h1 class='title is-3 has-text-centered'>Letter</h1>
+                    <h1 id='gameLetter' class='title is-1 has-text-centered'></h1>
+                    <h1 id="time" class="title is-4 has-text-centered">90</h1>
                     <button id="playButton" class="button">Play</button>
                 </div>
             </div>
             <div class="column is-three-fifths">
-                <h1 class='title is-3'>Categories</h1>
+                <h1 class='title is-3 has-text-centered'>Categories</h1>
                 {this.categories.map(cat => {
                     return <input class='input categories'></input>
                 })}
