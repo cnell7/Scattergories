8f612f98312962584e3697a2a9351b89bebb7d6d
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-22 22:55:16 -0500

    Remove typo

--- client/public/socket.js
+++ client/public/socket.js
@@ -98,7 +98,7 @@ socket.on('voting round', (game) => {
         htmlPlayersScore[counter].innerHTML = game.players[player];
         counter++;
     }
-A    document.getElementById('currentQuestion').innerHTML = game.currentCategories[game.currentVotingRound]
+    document.getElementById('currentQuestion').innerHTML = game.currentCategories[game.currentVotingRound]
 })
 
 socket.on('vote registered', () => {
