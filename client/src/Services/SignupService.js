510a60653519085ea4f667bd5e4e13788cee0b9d
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-24 11:21:10 -0500

    Edit url

--- client/src/Services/SignupService.js
+++ client/src/Services/SignupService.js
@@ -3,7 +3,7 @@ import * as axios from "../../node_modules/axios"
 export async function requestSignup(u, p) {
     const result = await axios({
         method: 'post',
-        url: 'http://localhost:3030/signup',
+        url: 'https://scattergories-app.herokuapp/signup',
         data: {
             user: u,
             password: p
